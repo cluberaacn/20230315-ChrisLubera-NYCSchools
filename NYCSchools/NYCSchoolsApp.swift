f7965e04b5de6b7e09b5e0999809c9cc1ecd8331
Chris Lubera <christopher.lubera@accenture.com>
2023-03-15 09:24:41 -0500

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by christopher.lubera on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
